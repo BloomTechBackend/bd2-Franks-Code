ae9c72ade351c4e284364ba66b85fff5a0734ef5
Frank Fella <frank.fella@lambdaschool.com>
2022-01-26 14:26:33 -0500

    FFella Push 01/26/2022 14:26:33

--- bd-mocking-rack-monitor-FrankJFella/src/com/amazon/ata/mocking/wholefoodscookieassembly/cookie/Size.java
+++
@@ -1,8 +0,0 @@
-package com.amazon.ata.mocking.wholefoodscookieassembly.cookie;
-
-/**
- * Size of cookies produced by the Whole Foods cookie production line.
- */
-public enum Size {
-    SMALL, MEDIUM, LARGE;
-}
